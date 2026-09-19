fa37797256676074640d53bd7375146fff1a2549
wpm-git <wpereiramaria@gmail.com>
2026-09-19 16:19:23 -0300

    Atualiza cache para links de referência clicáveis

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='cronogramas-pwa-v15';
+const CACHE='cronogramas-pwa-v16';
 const FILES=['./','./index.html','./manifest.webmanifest','./cronograma-192.png?v=13','./cronograma-512.png?v=13'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
